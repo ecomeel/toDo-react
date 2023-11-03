dc6b46e4b0c986b75ad4b4e88e5d674cad0fadb0
ecomeel <ecomeel@gmail.com>
2023-11-03 06:06:26 +0300

    mock todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+
+const mockTodos = [
+  {
+    id: 1,
+    title: 'delectes sd asd',
+    completed: false
+  },
+  {
+    id: 2,
+    title: 'ssfd wqr asdas',
+    completed: false
+  }
+]
 
 function App() {
+  const [todos, setTodos] = useState(null);
+  const [isTodosLoading, setIsTodosLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsTodosLoading(true)
+    setTodos(mockTodos)
+    setIsTodosLoading(false)
+  }, []);
+
+  console.log(todos)
+
   return (
+    <div>
+      <h1>Список задач</h1>
+      { isTodosLoading && <p>Todos Loading</p> }
+      { isError && <p>Error has occured</p> }
+      { todos && todos.map(todo => (
+        <p key={todo.id}>{todo.title}</p>
+      )) }
+    </div>
   )
 }
 
