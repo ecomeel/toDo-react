d11b21e28ef9dd1912a615da3ac0b813d29a6f89
ecomeel <ecomeel@gmail.com>
2023-11-03 06:25:08 +0300

    api to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,52 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import { getTodos } from "./api/todo";
 
-const mockTodos = [
-  {
-    id: 1,
-    title: 'delectes sd asd',
-    completed: false
-  },
-  {
-    id: 2,
-    title: 'ssfd wqr asdas',
-    completed: false
-  }
-]
+// const mockTodos = [
+//   {
+//     id: 1,
+//     title: 'delectes sd asd',
+//     completed: false
+//   },
+//   {
+//     id: 2,
+//     title: 'ssfd wqr asdas',
+//     completed: false
+//   }
+// ]
 
 function App() {
-  const [todos, setTodos] = useState(null);
-  const [isTodosLoading, setIsTodosLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+    const [todos, setTodos] = useState(null);
+    const [isTodosLoading, setIsTodosLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
-  useEffect(() => {
-    setIsError(false)
-    setIsTodosLoading(true)
+    useEffect(() => {
+        setIsError(false);
+        setIsTodosLoading(true);
 
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(data => console.log(data))
+        getTodos()
+            .then((todos) => {
+                setIsTodosLoading(false);
+                setTodos(todos);
+            })
+            .catch((error) => {
+                setIsError(true);
+                setIsTodosLoading(false);
+            });
 
-    setTodos(mockTodos)
-    setIsTodosLoading(false)
-  }, []);
+        setIsTodosLoading(false);
+    }, []);
 
-  console.log(todos)
+    console.log(todos);
 
-  return (
-    <div>
-      <h1>Список задач</h1>
-      { isTodosLoading && <p>Todos Loading</p> }
-      { isError && <p>Error has occured</p> }
-      { todos && todos.map(todo => (
-        <p key={todo.id}>{todo.title}</p>
-      )) }
-    </div>
-  )
+    return (
+        <div>
+            <h1>Список задач</h1>
+            {isTodosLoading && <p>Todos Loading</p>}
+            {isError && <p>Error has occured</p>}
+            {todos && todos.map((todo) => <p key={todo.id}>{todo.title}</p>)}
+        </div>
+    );
 }
 
-export default App
+export default App;
