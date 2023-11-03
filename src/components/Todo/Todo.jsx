aafe75ff41158609c90b6b3fd45f2cab4b17b7f2
ecomeel <ecomeel@gmail.com>
2023-11-03 10:34:19 +0300

    final build

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,15 +1,17 @@
+import './todo.css';
+
 export default function Todo({
     todo,
     onDelete,
     onToggle
 }) {
     return (
-        <div>
-            <p>{todo.title}</p>
+        <div className='todo'>
             <input 
                 type="checkbox"
                 checked={todo.completed}
                 onChange={onToggle} />
+            <p>{todo.title}</p>
             <button onClick={onDelete}>Delete todo</button>
         </div>
     )
